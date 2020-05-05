87cda9b4d0f5a35d1511f41ed35663b2201ce352
cody <cody.frank30@gmail.com>
2020-05-04 20:35:41 -0400

    counting valleys

--- solutions.rb
+++ solutions.rb
@@ -53,3 +53,40 @@ def array_diff(a, b)
 end
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
+# Counting Valleys HackerRank
+# Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During his last hike he took exactly  steps. For every step he took, he noted if it was an uphill, , or a downhill,  step. Gary's hikes start and end at sea level and each step up or down represents a  unit change in altitude. We define the following terms:
+
+# A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+# A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+# Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+
+# For example, if Gary's path is , he first enters a valley  units deep. Then he climbs out an up onto a mountain  units high. Finally, he returns to sea level and ends his hike.
+
+# Function Description
+
+# Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
+
+# countingValleys has the following parameter(s):
+
+# n: the number of steps Gary takes
+# s: a string describing his path
+
+# Solution
+
+def countingValleys(n, s)
+  elevation = 0
+  valley_count = 0
+  steps = s.split('')
+  steps.each do |s|
+      if s == "U"
+          elevation += 1
+      elsif s == "D" && elevation == 0
+          valley_count += 1
+          elevation -= 1
+      elsif s == "D"
+          elevation -= 1
+      end
+  end
+
+return valley_count
+end
