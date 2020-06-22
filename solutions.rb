d5a19824f1755431ed4701f362ce555f2895c987
Cody Frank <cody.frank30@gmail.com>
2020-06-22 14:37:55 -0400

    adds hackerrank 2d array solution

--- solutions.rb
+++ solutions.rb
@@ -151,3 +151,94 @@ def repeatedString(s, n)
   end
   
 end
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+#HackerRank 2d array
+# Given a  2D Array, :
+
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+# a b c
+#   d
+# e f g
+# There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+# For example, given the 2D array:
+
+# -9 -9 -9  1 1 1 
+#  0 -9  0  4 3 2
+# -9 -9 -9  1 2 3
+#  0  0  8  6 6 0
+#  0  0  0 -2 0 0
+#  0  0  1  2 4 0
+# We calculate the following  hourglass values:
+
+# -63, -34, -9, 12, 
+# -10, 0, 28, 23, 
+# -27, -11, -2, 10, 
+# 9, 17, 25, 18
+# Our highest hourglass value is  from the hourglass:
+
+# 0 4 3
+#   1
+# 8 6 6
+# Note: If you have already solved the Java domain's Java 2D Array challenge, you may wish to skip this challenge.
+
+# Function Description
+
+# Complete the function hourglassSum in the editor below. It should return an integer, the maximum hourglass sum in the array.
+
+# hourglassSum has the following parameter(s):
+
+# arr: an array of integers
+# Input Format
+
+# Each of the  lines of inputs  contains  space-separated integers .
+
+# Constraints
+
+# Output Format
+
+# Print the largest (maximum) hourglass sum found in .
+
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr)
+    new_arr = arr.flatten
+    hourglass_arr = []
+    i = 0
+  until i == 22
+    if i != 4 && i != 5 && i != 10 && i != 11 && i != 16 && i != 17
+      hourglass_sum = new_arr[i] + new_arr[i+1] + new_arr[i+2] + new_arr[i+7] + 
+      new_arr[i+12] + new_arr[i+13] + new_arr[i+14]
+      hourglass_arr << hourglass_sum
+    end
+    i += 1
+  end
+  return hourglass_arr.max
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+arr = Array.new(6)
+
+6.times do |i|
+    arr[i] = gets.rstrip.split(' ').map(&:to_i)
+end
+
+result = hourglassSum arr
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
